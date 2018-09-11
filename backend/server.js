62e527b261a8d7be46da85837c9393685059ef43
Lalith Kumar <lalith889@gmail.com>
2018-09-11 15:38:50 +0530

    added socket

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const path = require('path');
+const http =require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var publicpath = path.join (__dirname, '../frontend');
 var app = express();
 app.use(express.static(publicpath));
 
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection' ,(socket)=>{
+    console.log("new user connected");
+    socket.on('disconnect', ()=>{
+        console.log("client disconnected from server");
+    })
+})
 
 
 
 const port = process.env.PORT || 3000 ;
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log('app running on localhost' + port);
 })
