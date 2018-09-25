d468bdfc33689fec28d96aea4340a3dde28924f4
Lalith Kumar <lalith889@gmail.com>
2018-09-25 22:26:50 +0530

    'dummy'

--- backend/server.js
+++ backend/server.js
@@ -5,31 +5,53 @@ const socketIO = require('socket.io');
 var generatemessage = require('./utilits/message');
 var publicpath = path.join (__dirname, '../frontend');
 var app = express();
-app.use(express.static(publicpath));
+// app.use(express.static(publicpath));
 
-var server = http.createServer(app);
-var io = socketIO(server);
+// var server = http.createServer(app);
+// var io = socketIO(server);
 
-io.on('connection' ,(socket)=>{
-    console.log("new user connected");
-    socket.emit('dummydata', generatemessage('admin', 'Hia you are connected to our server'));
-    socket.broadcast.emit('dummydata', generatemessage('admin', 'Hey !! someone connected'));
+// io.on('connection' ,(socket)=>{
+//     console.log("new user connected");
+//     socket.emit('dummydata', generatemessage('admin', 'Hia you are connected to our server'));
+//     socket.broadcast.emit('dummydata', generatemessage('admin', 'Hey !! someone connected'));
 
-    socket.on('createmail', (data , ack)=>{
-        console.log(data);
-        socket.broadcast.emit('recpit', generatemessage(data.from ,data.text , data.time));
-        ack(generatemessage(data.from ,data.text , data.time));
-    });
+//     socket.on('createmail', (data , ack)=>{
+//         console.log(data);
+//         socket.broadcast.emit('recpit', generatemessage(data.from ,data.text , data.time));
+//         ack(generatemessage(data.from ,data.text , data.time));
+//     });
 
-    socket.on('disconnect', ()=>{
-        console.log("client disconnected from server");
-    })
-});
+//     socket.on('disconnect', ()=>{
+//         console.log("client disconnected from server");
+//     })
+// });
 
 
 
+
+app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if(req.header('idd') == '1')
+    { 
+        console.log(req.header('idd'));
+        next();
+    }
+    else {
+        console.log(req.body);
+        return res.status(501).send("http intersecpetor failed ");
+    }
+    // next();
+})
+
+
+
+app.get('/insert',(req,res)=>{
+    res.send({status:'Hi You reached server'});
+})
 
 const port = process.env.PORT || 3000 ;
-server.listen(port, ()=>{
+app.listen(port, ()=>{
     console.log('app running on localhost' + port);
 })
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -6,16 +6,20 @@ socket.on('disconnect', function () {
     console.log('disconnected from server');
 });
 socket.on('recpit' ,function (data) {
-    var li = $('<li style="text-align:left; width:250px; list-style-type: none;"></li>');
+    var li = $('<li style="text-align:left; list-style-type: none;"></li>');
     li.html(`<h4 style="margin:0px">${data.text} </h4><span style="font-size:10px;">${data.time}</span>`);
     $('#messages').append(li);
+    var msgList = document.getElementById("msg-list");
+    msgList.scrollTop = msgList.scrollHeight;
 });
 
 function sendmessage() {
     socket.emit('createmail', {from:'tab',text:document.getElementById('message').value},function(ack){
-        var li = $('<li style="text-align:right; width:250px; list-style-type: none;"></li>');
+        var li = $('<li style="text-align:right; list-style-type: none;"></li>');
         li.html(`<h4 style="margin:0px">${ack.text} </h4><span style="font-size:10px;">${ack.time}</span>`);
         $('#messages').append(li);
+        var msgList = document.getElementById("msg-list");
+        msgList.scrollTop = msgList.scrollHeight;
     });
 
 
